d6d09292725616f2d1a87602238b7285b6a8d1de
pappisJ <pappesd@gmail.com>
2023-05-28 22:32:13 +0300

    ghfrdrgrj

--- eLesxhClient.java
+++ eLesxhClient.java
@@ -4,10 +4,12 @@ import java.net.UnknownHostException;
 import CONNECTION.CLIENT.Client;
 
 public class eLesxhClient {
-    
-    public static void main(String args[]) throws UnknownHostException, IOException, ClassNotFoundException, InterruptedException {
-        
+
+    public static void main(String args[])
+            throws UnknownHostException, IOException, ClassNotFoundException, InterruptedException {
+
         new Client();
-        
+        // dgfsef
+
     }
 }
