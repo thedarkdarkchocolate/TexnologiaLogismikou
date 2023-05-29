416d6f8489b72d0a5dab8c06c377de62bb67322c
pappisJ <pappesd@gmail.com>
2023-05-29 18:04:03 +0300

    changes

--- eLesxhClient.java
+++ eLesxhClient.java
@@ -9,8 +9,6 @@ public class eLesxhClient {
             throws UnknownHostException, IOException, ClassNotFoundException, InterruptedException {
 
         new Client();
-        // dgfsef
 
-        // fsfsefsgh
     }
 }
